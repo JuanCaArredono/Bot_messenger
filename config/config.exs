da12b05aaba45b0bed3ebc667875184e261717fa
JuanCaArredono <juan.arredondo@diverza.com>
2022-11-08 22:40:38 +0000

    first commit

---
+++ config/config.exs
@@ -0,0 +1,35 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :elixirbot,
+  facebook_chat_bot: %{
+    base_url: "https://graph.facebook.com",
+    page_access_token: System.get_env("FACEBOOK_PAGE_ACCESS_TOKEN"),
+    webhook_verify_token: System.get_env("FACEBOOK_WEBHOOK_VERIFY_TOKEN")
+  }
+
+# Configures the endpoint
+config :elixirbot, ElixirbotWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "/87wwG5mpDY5QKj8+m58aaKi8wli7NeP8UjBUKR9BeDoIv68hs6EjD2w0YTncewD",
+  render_errors: [view: ElixirbotWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Elixirbot.PubSub,
+  live_view: [signing_salt: "dgIbYwj3"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
---
+++ lib/elixirbot_web/router.ex
@@ -0,0 +1,29 @@
+defmodule ElixirbotWeb.Router do
+  use ElixirbotWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", TomatoWeb do
+    pipe_through :api
+    get "/facebook_webhook", FacebookController, :verify_webhook_token
+    post "/facebook_webhook", FacebookController, :handle_message
+  end
+
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+      live_dashboard "/dashboard", metrics: ElixirbotWeb.Telemetry
+    end
+  end
+end
